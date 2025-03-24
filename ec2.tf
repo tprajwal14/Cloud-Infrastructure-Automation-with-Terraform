640e0be410a62144bda70166c5a376a2c801fd29
Prajwal Take <takeprajwal4@gmail.com>
2025-03-24 10:12:18 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                         = "ami-04b4f1a9cf54c11d0"  
+  ami                         = "ami-04b4f1a9cf54c11d0"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   key_name                    = aws_key_pair.tf-key-pair.key_name
